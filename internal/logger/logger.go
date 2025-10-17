8fee4c9521f31ee1a2197dfce427b9f6ea107308
Aleks <xzerox23@gmail.com>
2025-10-17 15:51:38 +0300

    Add realization of logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,7 +1,39 @@
 package logger
 
+import (
+	"log"
+	"log/slog"
+	"os"
+)
+
 type Logger interface {
 	Info(string)
 	Debug(string)
 	Error(string)
+}
+
+type StdLogger struct {
+	logger *slog.Logger
+}
+
+func NewLogger() *StdLogger {
+	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &StdLogger{
+		logger: slog.New(slog.NewTextHandler(file, nil)),
+	}
+}
+
+func (s *StdLogger) Info(msg string) {
+	s.logger.Info(msg)
+}
+
+func (s *StdLogger) Debug(msg string) {
+	s.logger.Debug(msg)
+}
+
+func (s *StdLogger) Error(msg string) {
+	s.logger.Error(msg)
 }
