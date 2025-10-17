343740728b128eecfce7e0c27869b49ed16b6ee5
Aleks <xzerox23@gmail.com>
2025-10-17 14:13:14 +0300

    add realization of in memory storage

--- internal/service/service.go
+++ internal/service/service.go
@@ -17,6 +17,32 @@ func NewUrlShortner(logger logger.Logger, storage storage.Storage) *UrlShortener
 	}
 }
 
-func Create(url string) (string, error) {
-	return "", nil
+func (s *UrlShortener) Create(url string) (string, error) {
+	s.logger.Info("creating url")
+	short, err := s.storage.Create(url)
+	if err != nil {
+		s.logger.Error("creating url error")
+		return "", err
+	}
+	return short, nil
+}
+
+func (s *UrlShortener) Get(url string) (string, error) {
+	s.logger.Info("getting url")
+	long, err := s.storage.Get(url)
+	if err != nil {
+		s.logger.Error("getting url error")
+		return "", err
+	}
+	return long, nil
+}
+
+func (s *UrlShortener) Delete(url string) error {
+	s.logger.Info("deleting url")
+	err := s.storage.Delete(url)
+	if err != nil {
+		s.logger.Error("error deleting url")
+		return err
+	}
+	return nil
 }
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,48 @@
 package storage
 
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+)
+
 type Storage interface {
 	Get(url string) (string, error)
 	Create(url string) (string, error)
 	Delete(url string) error
+}
+
+type shortUrl string
+type longUrl string
+
+type InMemory struct {
+	cache map[shortUrl]longUrl
+}
+
+func (s *InMemory) Create(url string) (string, error) {
+	hash := getMD5Hash(url)
+	s.cache[shortUrl(hash)] = longUrl(url)
+	return hash, nil
+}
+
+func (s *InMemory) Get(url string) (string, error) {
+	result, ok := s.cache[shortUrl(url)]
+	if !ok {
+		return "", fmt.Errorf("invalid url")
+	}
+	return string(result), nil
+}
+
+func (s *InMemory) Delete(url string) error {
+	if _, ok := s.cache[shortUrl(url)]; !ok {
+		return fmt.Errorf("no such url")
+	}
+	delete(s.cache, shortUrl(url))
+	return nil
+}
+
+func getMD5Hash(url string) string {
+	hash := md5.Sum([]byte(url))
+	return hex.EncodeToString(hash[:])
+
 }
